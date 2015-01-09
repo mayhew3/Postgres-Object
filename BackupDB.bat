675454f65833e9ad8c8095573aebbd3a880b8929
Mayhew Seavey <scorpy@gmail.com>
2015-01-09 13:02:30 -0800

    DB Backup scripts

---
+++ BackupDB.bat
@@ -0,0 +1,20 @@
+setlocal
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a_%%b)
+set fulldatetime=%mydate%_%mytime%
+set fullBackupPath=D:\Projects\mean_projects\backups\%fulldatetime%
+echo %fullBackupPath%
+set mongoshutdown="0"
+tasklist /FI "IMAGENAME eq mongod.exe" 2>NUL | find /I /N "mongod.exe">NUL
+if "%ERRORLEVEL%"=="0" goto Running
+echo MongoDB isn't started yet. Starting.
+set mongoshutdown="1"
+start "Mongo Games" D:\Projects\mean\mongodb\bin\mongod.exe -dbpath D:\Projects\mean_projects\GameLord\data\db
+echo DB started.
+:Running
+echo Backing up
+
+call mongodump --out %fullBackupPath%
+
+if %mongoshutdown%=="1" taskkill /im mongod.exe
+endlocal
---
+++ SimpleBackup.bat
@@ -0,0 +1,2 @@
+@echo off
+call mongodump --db tv --out "D:/Projects/mean_projects/backups/TV_2015_01_09_12_09"
