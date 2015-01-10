0a61283e45f47ce779637aa274e60a06b8cdfc28
Mayhew Seavey <scorpy@gmail.com>
2015-01-10 00:48:37 -0800

    Fix issue in backup script where a space was being made when the hour was 0, because for some reason it's not 00.

--- DateTimeTest.bat
+++ DateTimeTest.bat
@@ -1,6 +1,6 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a_%%b)
+For /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (set mytime=%%a_%%b)
 set fulldatetime=%mydate%_%mytime%
 set fullBackupPath=D:\Projects\mean_projects\backups\TV\%fulldatetime%
 echo %fullBackupPath%
