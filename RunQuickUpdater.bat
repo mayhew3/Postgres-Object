c6715e2a4261715ae87195286a91a3289bbb5e5f
Mayhew Seavey <scorpy@gmail.com>
2015-01-30 01:54:08 -0800

    Add quick updater batch file

---
+++ RunQuickUpdater.bat
@@ -0,0 +1,14 @@
+setlocal
+set mongoshutdown="0"
+tasklist /FI "IMAGENAME eq mongod.exe" 2>NUL | find /I /N "mongod.exe">NUL
+if "%ERRORLEVEL%"=="0" goto Running
+echo MongoDB isn't started yet. Starting.
+set mongoshutdown="1"
+start "Mongo Games" D:\Projects\mean\mongodb\bin\mongod.exe -dbpath D:\Projects\mean_projects\GameLord\data\db
+echo DB started.
+:Running
+echo Program is running
+java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.SteamGameUpdater
+java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.TiVoLibraryUpdater TiVoOnly
+if %mongoshutdown%=="1" taskkill /im mongod.exe
+endlocal
