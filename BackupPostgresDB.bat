2d0d72fd0967007d32afeb282e8243392bbb1d29
Mayhew Seavey <scorpy@gmail.com>
2016-04-20 01:01:34 -0700

    MM-161: Backup local and heroku batches now both work. With some effort.

--- BackupPostgresDB.bat
+++ BackupPostgresDB.bat
@@ -3,9 +3,13 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
 For /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (set mytime=%%a_%%b)
 set fulldatetime=%mydate%_%mytime%
 set fullBackupPath=D:\Projects\mean_projects\backups_postgres\%fulldatetime%
+
+set PGPASSFILE=%postgres_pgpass_heroku%
+echo %PGPASSFILE%
+
 echo %fullBackupPath%
 echo Backing up
 
-call pg_dump --file=%fullBackupPath%.dump --format=custom
+call pg_dump --host=ec2-54-204-30-115.compute-1.amazonaws.com --dbname=dcg1rtode4vgk9 --username=ijxuheufucvnyu --file=%fullBackupPath%.dump --format=custom
 
 endlocal
---
+++ BackupPostgresLocalDB.bat
@@ -0,0 +1,15 @@
+setlocal
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
+For /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (set mytime=%%a_%%b)
+set fulldatetime=%mydate%_%mytime%
+set fullBackupPath=D:\Projects\mean_projects\backups_postgres_local\%fulldatetime%
+
+set PGPASSFILE=%postgres_pgpass_local%
+echo %PGPASSFILE%
+
+echo %fullBackupPath%
+echo Backing up
+
+call pg_dump --host=localhost --dbname=tv --username=postgres --file=%fullBackupPath%.dump --format=custom
+
+endlocal
