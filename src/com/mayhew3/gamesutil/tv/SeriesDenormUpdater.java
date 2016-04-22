9c20fd8524c3d07312fa362ac452e74a5a3fbba4
Mayhew Seavey <scorpy@gmail.com>
2016-04-22 01:30:39 -0700

    MM-154, MM-59: Add missing DBCC updaters so they are run at the end of the update

--- src/com/mayhew3/gamesutil/tv/SeriesDenormUpdater.java
+++ src/com/mayhew3/gamesutil/tv/SeriesDenormUpdater.java
@@ -27,6 +27,87 @@ public class SeriesDenormUpdater {
     updateUnwatchedEpisodes();
     updateLastUnwatched();
     updateMostRecent();
+    updateDeletedEpisodes();
+    updateSuggestionEpisodes();
+    updateWatchedEpisodes();
+    updateMatchedEpisodes();
+    updateTVDBOnlyEpisodes();
+    updateUnwatchedUnrecorded();
+  }
+
+  private void updateUnwatchedUnrecorded() throws SQLException {
+    connection.prepareAndExecuteStatementUpdate(
+        "update series\n" +
+            "set unwatched_unrecorded = (select count(1)\n" +
+            "                            from episode e                            \n" +
+            "                            where e.seriesid = series.id\n" +
+            "                            and e.on_tivo = ?\n" +
+            "                            and e.watched = ?\n" +
+            "                            and e.season <> ?\n" +
+            "                            and e.retired = ?)",
+        false, false, 0, 0
+    );
+  }
+
+  private void updateTVDBOnlyEpisodes() throws SQLException {
+    connection.prepareAndExecuteStatementUpdate(
+        "update series\n" +
+            "set tvdb_only_episodes = (select count(1)\n" +
+            "                            from episode e                            \n" +
+            "                            where e.seriesid = series.id\n" +
+            "                            and e.on_tivo = ?\n" +
+            "                            and e.season <> ?\n" +
+            "                            and e.retired = ?)",
+        false, 0, 0
+    );
+  }
+
+  private void updateMatchedEpisodes() throws SQLException {
+    connection.prepareAndExecuteStatementUpdate(
+        "update series\n" +
+            "set matched_episodes = (select count(1)\n" +
+            "                            from episode e                            \n" +
+            "                            where e.seriesid = series.id\n" +
+            "                            and e.on_tivo = ?\n" +
+            "                            and e.season <> ?\n" +
+            "                            and e.retired = ?)",
+        true, 0, 0
+    );
+  }
+
+  private void updateWatchedEpisodes() throws SQLException {
+    connection.prepareAndExecuteStatementUpdate(
+        "update series\n" +
+            "set watched_episodes = (select count(1)\n" +
+            "                            from episode e                            \n" +
+            "                            where e.seriesid = series.id\n" +
+            "                            and e.watched = ?\n" +
+            "                            and e.season <> ?\n" +
+            "                            and e.retired = ?)",
+        true, 0, 0
+    );
+  }
+
+  private void updateSuggestionEpisodes() throws SQLException {
+    connection.prepareAndExecuteStatementUpdate(
+        "update series\n" +
+            "set suggestion_episodes = (select count(1)\n" +
+            "                            from episode e\n" +
+            "                            left outer join edge_tivo_episode ete\n" +
+            "                             on ete.episode_id = e.id\n" +
+            "                            left outer join tivo_episode te\n" +
+            "                             on ete.tivo_episode_id = te.id\n" +
+            "                            where e.seriesid = series.id\n" +
+            "                            and e.on_tivo = ?\n" +
+            "                            and te.id is not null\n" +
+            "                            and e.season <> ?\n" +
+            "                            and te.suggestion = ?\n" +
+            "                            and te.deleted_date is null\n" +
+            "                            and e.retired = ?\n" +
+            "                            and ete.retired = ?\n" +
+            "                            and te.retired = ?)",
+        true, 0, true, 0, 0, 0
+    );
   }
 
   private void updateMostRecent() throws SQLException {
@@ -45,7 +126,9 @@ public class SeriesDenormUpdater {
             "                            and te.deleted_date is null\n" +
             "                            and e.season <> ?\n" +
             "                            and e.retired = ?\n" +
-            "                            and te.retired = ?)", true, true, 0, 0, 0
+            "                            and te.retired = ? " +
+            "                            and ete.retired = ?)",
+        true, true, 0, 0, 0, 0
     );
   }
 
@@ -66,7 +149,9 @@ public class SeriesDenormUpdater {
             "                            and e.watched <> ?\n" +
             "                            and e.season <> ?\n" +
             "                            and e.retired = ?\n" +
-            "                            and te.retired = ?)", true, true, true, 0, 0, 0
+            "                            and te.retired = ? " +
+            "                            and ete.retired = ? )",
+        true, true, true, 0, 0, 0, 0
     );
   }
 
@@ -87,7 +172,9 @@ public class SeriesDenormUpdater {
             "                            and e.watched <> ?\n" +
             "                            and e.season <> ?\n" +
             "                            and e.retired = ?\n" +
-            "                            and te.retired = ?)", true, true, true, 0, 0, 0);
+            "                            and ete.retired = ?\n" +
+            "                            and te.retired = ?)",
+        true, true, true, 0, 0, 0, 0);
   }
 
   private void updateActiveEpisodes() throws SQLException {
@@ -106,7 +193,29 @@ public class SeriesDenormUpdater {
             "                            and e.season <> ?\n" +
             "                            and te.deleted_date is null\n" +
             "                            and e.retired = ?\n" +
-            "                            and te.retired = ?)", true, false, 0, 0, 0);
+            "                            and ete.retired = ?\n" +
+            "                            and te.retired = ?)",
+        true, false, 0, 0, 0, 0);
+  }
+
+  private void updateDeletedEpisodes() throws SQLException {
+    connection.prepareAndExecuteStatementUpdate(
+        "update series\n" +
+            "set deleted_episodes = (select count(1)\n" +
+            "                            from episode e\n" +
+            "                            left outer join edge_tivo_episode ete\n" +
+            "                             on ete.episode_id = e.id\n" +
+            "                            left outer join tivo_episode te\n" +
+            "                             on ete.tivo_episode_id = te.id\n" +
+            "                            where e.seriesid = series.id\n" +
+            "                            and e.on_tivo = ?\n" +
+            "                            and te.id is not null\n" +
+            "                            and e.season <> ?\n" +
+            "                            and te.deleted_date is not null\n" +
+            "                            and e.retired = ?\n" +
+            "                            and ete.retired = ?\n" +
+            "                            and te.retired = ?)",
+        true, 0, 0, 0, 0);
   }
 
   private void updateUnmatchedEpisodes() throws SQLException {
@@ -116,7 +225,8 @@ public class SeriesDenormUpdater {
             "                            from tivo_episode te\n" +
             "                            where not exists (select 1 from edge_tivo_episode ete where ete.tivo_episode_id = te.id and retired = ?)\n" +
             "                            and te.tivo_series_id = series.tivo_series_id\n" +
-            "                            and te.retired = ?)", 0, 0);
+            "                            and te.retired = ?)",
+        0, 0);
   }
 
 }
