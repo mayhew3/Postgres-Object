bd58fc00c6acf1b600492d908b52b92c106d4268
Mayhew Seavey <scorpy@gmail.com>
2016-04-25 22:00:03 -0700

    Make Copy utility cooler by actually backing up prod and restoring it immediately into test.

--- CopyPostgresLocalToTestDB.bat
+++ CopyPostgresLocalToTestDB.bat
@@ -1,11 +1,18 @@
 setlocal
-set fullBackupPath=D:\Projects\mean_projects\backups_postgres_local\2016_04_22_0_09
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
+For /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (set mytime=%%a_%%b)
+set fulldatetime=%mydate%_%mytime%
+set fullBackupPath=D:\Projects\mean_projects\backups_postgres_local\%fulldatetime%
 
 set PGPASSFILE=%postgres_pgpass_local%
 echo %PGPASSFILE%
 
 echo %fullBackupPath%
-echo Restoring
+echo Backing up
+
+call pg_dump --host=localhost --dbname=tv --username=postgres --file=%fullBackupPath%.dump --format=custom
+
+echo Restoring backup
 
 call pg_restore --host=localhost --username=postgres --dbname=tv_copy --clean --verbose --format=custom %fullBackupPath%.dump
 
