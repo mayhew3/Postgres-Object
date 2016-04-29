84fc666b3c4c308e0845ae21f25685238083b041
Mayhew Seavey <scorpy@gmail.com>
2016-04-29 00:06:25 -0700

    MM-185: Add DB arguments to my batch files.

--- RunUpdater.bat
+++ RunUpdater.bat
@@ -8,9 +8,9 @@ start "Mongo Games" D:\Projects\mean\mongodb\bin\mongod.exe -dbpath D:\Projects\
 echo DB started.
 :Running
 echo Program is running
-java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" -Dwebdriver.chrome.driver="C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe" com.mayhew3.gamesutil.games.SteamGameUpdater LogToFile
-java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.games.MetacriticGameUpdateRunner LogToFile
+java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" -Dwebdriver.chrome.driver="C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe" com.mayhew3.gamesutil.games.SteamGameUpdater LogToFile Heroku
+java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.games.MetacriticGameUpdateRunner LogToFile Heroku
 java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.tv.mongo.TiVoLibraryUpdater FullMode LogToFile
-java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.tv.TiVoLibraryUpdater FullMode LogToFile
+java -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.tv.TiVoLibraryUpdater FullMode LogToFile Local
 if %mongoshutdown%=="1" taskkill /im mongod.exe
 endlocal
