82fca26a058cb52c4ebc324eeded766570446c11
Mayhew Seavey <commander.boffo@gmail.com>
2016-11-20 17:08:51 -0800

    Update Dreamatorium environment

---
+++ RestorePostgresLocalDB_DR.bat
@@ -0,0 +1,12 @@
+setlocal
+set fullBackupPath=D:\Dropbox\Projects\MediaMogul\2016_11_20_16_19
+
+set PGPASSFILE=%postgres_pgpass_local%
+echo %PGPASSFILE%
+
+echo %fullBackupPath%
+echo Restoring
+
+call pg_restore --host=localhost --username=postgres --dbname=tv --clean --verbose --format=custom %fullBackupPath%.dump
+
+endlocal
