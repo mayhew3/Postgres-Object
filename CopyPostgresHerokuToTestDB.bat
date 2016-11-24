0acb34bfb7e766a7918c00bf52500d9f0fe99c13
Mayhew Seavey <scorpy@gmail.com>
2016-11-23 20:06:16 -0800

    MM-10: Make DB backup utilities work with Heroku

--- CopyPostgresHerokuToTestDB.bat
+++ CopyPostgresHerokuToTestDB.bat
@@ -4,13 +4,16 @@ For /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (set mytime=%%a_%%b)
 set fulldatetime=%mydate%_%mytime%
 set fullBackupPath=D:\Projects\mean_projects\backups_postgres_heroku\%fulldatetime%
 
-set PGPASSFILE=%postgres_pgpass_local%
+set PGPASSFILE=%postgres_pgpass_heroku%
 echo %PGPASSFILE%
 
 echo %fullBackupPath%
 echo Backing up
 
 call pg_dump --host=ec2-23-23-199-72.compute-1.amazonaws.com --dbname=dlo6ch087btcf --username=hwubispysgwomv --file=%fullBackupPath%.dump --format=custom
+
+set PGPASSFILE=%postgres_pgpass_local%
+echo %PGPASSFILE%
 
 echo Restoring backup
 
