49df6b36c2f9ff09588bb21fde37933f38d8ac06
Mayhew Seavey <scorpy@gmail.com>
2017-02-10 22:48:52 -0800

    MM-297: Forgot to mark the Finder with LogToFile.

--- RunTVDBFinderHeroku.bat
+++ RunTVDBFinderHeroku.bat
@@ -1,5 +1,5 @@
 setlocal
 :Running
 echo Program is running
-start javaw -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.tv.TVDBUpdateFinder Test
+start javaw -classpath "D:\Projects\mean_projects\GamesDBUtil\out\artifacts\GamesDBUtil_jar\GamesDBUtil.jar" com.mayhew3.gamesutil.tv.TVDBUpdateFinder Test LogToFile
 endlocal
