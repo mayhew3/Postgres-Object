6bec9b94141dc8847ce36abb4711a20e137e71b7
Mayhew Seavey <commander.boffo@gmail.com>
2017-10-25 18:42:44 -0700

    MM-205: Reconfigure episode_rating table to handle all person viewing an episode correlations.

--- src/main/java/com/mayhew3/mediamogul/model/tv/EpisodeRating.java
+++ src/main/java/com/mayhew3/mediamogul/model/tv/EpisodeRating.java
@@ -1,18 +1,22 @@
 package com.mayhew3.mediamogul.model.tv;
 
 import com.mayhew3.mediamogul.dataobject.*;
+import com.mayhew3.mediamogul.model.Person;
 
 public class EpisodeRating extends RetireableDataObject {
 
   public FieldValueForeignKey episodeId = registerForeignKey(new Episode(), Nullability.NOT_NULL);
+  public FieldValueForeignKey personId = registerForeignKey(new Person(), Nullability.NOT_NULL);
 
-  public FieldValueTimestamp ratingDate = registerTimestampField("rating_date", Nullability.NOT_NULL);
+  public FieldValueBoolean watched = registerBooleanField("watched", Nullability.NOT_NULL).defaultValue(true);
+  public FieldValueTimestamp watchedDate = registerTimestampField("watched_date", Nullability.NULLABLE);
+  public FieldValueTimestamp ratingDate = registerTimestampField("rating_date", Nullability.NULLABLE);
 
   public FieldValueBigDecimal ratingFunny = registerBigDecimalField("rating_funny", Nullability.NULLABLE);
   public FieldValueBigDecimal ratingCharacter = registerBigDecimalField("rating_character", Nullability.NULLABLE);
   public FieldValueBigDecimal ratingStory = registerBigDecimalField("rating_story", Nullability.NULLABLE);
 
-  public FieldValueBigDecimal ratingValue = registerBigDecimalField("rating_value", Nullability.NOT_NULL);
+  public FieldValueBigDecimal ratingValue = registerBigDecimalField("rating_value", Nullability.NULLABLE);
 
   public FieldValueString review = registerStringField("review", Nullability.NULLABLE);
 
