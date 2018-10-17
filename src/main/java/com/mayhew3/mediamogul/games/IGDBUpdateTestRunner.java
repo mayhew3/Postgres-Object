2e2d59500c17a7c141620af1c28fb0ab0e296ec4
Mayhew Seavey <commander.boffo@gmail.com>
2018-10-17 13:42:54 -0700

    MM-519: See if upgrading or using simple request in its own program completes. It doesn't.

---
+++ src/main/java/com/mayhew3/mediamogul/games/IGDBUpdateTestRunner.java
@@ -0,0 +1,49 @@
+package com.mayhew3.mediamogul.games;
+
+import callback.OnSuccessCallback;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import wrapper.IGDBWrapper;
+import wrapper.Parameters;
+import wrapper.Version;
+
+public class IGDBUpdateTestRunner {
+
+
+  /*
+  
+    Test utility to see if threads close in most basic case. They don't.
+
+  * */
+  public static void main(String[] args) {
+    IGDBWrapper igdbWrapper = new IGDBWrapper(System.getenv("igdb_key"), Version.STANDARD, false);
+
+    Parameters parameters = new Parameters()
+        .addSearch("Forza Horizon 4")
+        .addFields("name,cover")
+        .addLimit("5")
+        .addOffset("0");
+
+    igdbWrapper.games(parameters, new OnSuccessCallback() {
+      @Override
+      public void onSuccess(@NotNull JSONArray jsonArray) {
+        System.out.println(jsonArray);
+      }
+
+      @Override
+      public void onError(@NotNull Exception e) {
+        throw new RuntimeException(e);
+      }
+    });
+
+
+
+  }
+
+  protected static void debug(Object object) {
+    System.out.println(object);
+  }
+
+
+
+}
