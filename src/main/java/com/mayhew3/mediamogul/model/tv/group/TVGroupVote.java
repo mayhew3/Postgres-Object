7fb2d1a79d147850cabb764718ee21519c03d297
Mayhew Seavey <commander.boffo@gmail.com>
2018-11-23 16:50:24 -0800

    MM-559: Add missing unique constraint

--- src/main/java/com/mayhew3/mediamogul/model/tv/group/TVGroupVote.java
+++ src/main/java/com/mayhew3/mediamogul/model/tv/group/TVGroupVote.java
@@ -1,5 +1,6 @@
 package com.mayhew3.mediamogul.model.tv.group;
 
+import com.mayhew3.mediamogul.dataobject.FieldValueForeignKey;
 import com.mayhew3.mediamogul.dataobject.Nullability;
 import com.mayhew3.mediamogul.dataobject.RetireableDataObject;
 import com.mayhew3.mediamogul.model.Person;
@@ -7,10 +8,12 @@ import com.mayhew3.mediamogul.model.Person;
 public class TVGroupVote extends RetireableDataObject {
 
   public TVGroupVote() {
-    registerForeignKey(new TVGroupBallot(), Nullability.NOT_NULL);
-    registerForeignKey(new Person(), Nullability.NOT_NULL);
+    FieldValueForeignKey ballot_id = registerForeignKey(new TVGroupBallot(), Nullability.NOT_NULL);
+    FieldValueForeignKey person_id = registerForeignKey(new Person(), Nullability.NOT_NULL);
     registerIntegerField("vote_value", Nullability.NULLABLE);
     registerBooleanField("vote_skipped", Nullability.NOT_NULL).defaultValue(false);
+
+    addUniqueConstraint(ballot_id, person_id, retired);
   }
 
   @Override
