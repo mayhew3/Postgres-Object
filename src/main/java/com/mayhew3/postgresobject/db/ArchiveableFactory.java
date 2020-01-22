584bcc290a7f921f62cf3155dde1de81960d4852
mayhew3 <commander.boffo@gmail.com>
2020-01-22 12:50:48 -0800

    MA-100: Move archiver to library

---
+++ src/main/java/com/mayhew3/postgresobject/db/ArchiveableFactory.java
@@ -0,0 +1,10 @@
+package com.mayhew3.postgresobject.db;
+
+import com.mayhew3.postgresobject.dataobject.DataObject;
+
+public abstract class ArchiveableFactory<T extends DataObject> {
+  public abstract T createEntity();
+  public abstract Integer monthsToKeep();
+  public abstract String tableName();
+  public abstract String dateColumnName();
+}
